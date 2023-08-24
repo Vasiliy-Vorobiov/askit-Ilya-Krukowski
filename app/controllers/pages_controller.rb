0b40fb95adbd30dd9d98edbd3630591ab06975ac
Vasiliy <135667979+Vasiliy-Vorobiov@users.noreply.github.com>
2023-08-24 15:36:24 +0300

    created DB postgre, update yarn, new controller <pages>

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+  def index
+    # Выводим страницу index (расширение не нужно) из папки pages
+    # render - метод, который вызывается для отрисовки страницы
+    # render - по умолчанию вызывается index.html.erb для render :action => 'index'
+    @username = params[:username]
+  end
+end

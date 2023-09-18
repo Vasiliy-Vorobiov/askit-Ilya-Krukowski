465aab4271ea456236eb5b0fb1ed9a3f9723c012
Vasiliy <135667979+Vasiliy-Vorobiov@users.noreply.github.com>
2023-09-19 00:03:14 +0300

    Простой Pagination с помощью kaminari

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
   before_action :set_question!, only: %i[show edit update destroy]
 
   def index
-    @questions = Question.all
+    @questions = Question.order(created_at: 'DESC').page params[:page]
   end
 
   def new

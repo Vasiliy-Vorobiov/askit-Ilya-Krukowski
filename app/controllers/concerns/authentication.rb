29500fccd842ec0f4a433d037efefbc6cfa2114f
Vasiliy <135667979+Vasiliy-Vorobiov@users.noreply.github.com>
2023-11-12 18:08:08 +0300

    Проверка, вошёл ли пользователь в систему в before_action

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -14,6 +14,20 @@ module Authentication
       current_user.present?
     end
 
+    def require_authentication
+      return if user_signed_in?
+
+      flash[:warning] = 'You are not signed in'
+      redirect_to root_path
+    end
+
+    def require_no_authentication
+      return unless user_signed_in?
+
+      flash[:warning] = 'You are alredy sign in'
+      redirect_to root_path
+    end
+
     def sign_in(user)
       session[:user_id] = user.id
     end

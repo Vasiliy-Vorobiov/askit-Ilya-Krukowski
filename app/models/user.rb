dced9148025a4a291c78be9fbe3a3dd1f00be035
Vasiliy-Vorobiov <135667979+Vasiliy-Vorobiov@users.noreply.github.com>
2023-11-15 09:10:34 +0300

    40:00 Проверка корректности e-mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
   validate :correct_old_password, on: :update
   validates :password, confirmation: true, allow_blank: true, length: { minimum: 8, maximum: 70 }
 
-  validates :email, presence: true, uniqueness: true
-  # validate :password_complexity
+  validates :email, presence: true, uniqueness: true, 'valid_email_2/email': true
+  # validate :password_complexity # пока отключен, чтобы не вводить сложный пароль
 
   private
 
